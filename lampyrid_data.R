781d45d68fd8d33c92095d5000a34b3fdbe86448
saraherm <slh@msu.edu>
2016-02-23 16:26:07 -0500

    added weather data, made plots

--- lampyrid_data.R
+++ lampyrid_data.R
@@ -7,3 +7,12 @@ lampyrid$newdate <- mdy(lampyrid$DATE)
 #wow that was magic! by calling the new vector lampyrid$newdate, the new vector of dates is created as a column within the loaded dataset.
 lampyrid$year <- year(lampyrid$newdate) #create a column for year
 lampyrid$doy <- yday(lampyrid$newdate)
+
+#download weather data from KBS weather station
+weather<-read.table(file="http://lter.kbs.msu.edu/datatables/7.csv", 
+                    header=T, sep=",", na.strings="")
+weather$doy <- yday(weather$date)
+plot(weather$air_temp_mean, weather$doy)
+plot(weather$doy, weather$air_temp_mean)
+plot(weather$doy, weather$precipitation)
+
